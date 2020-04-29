7ec810b0d553715f9ed109419811e9c642ff7058
Ian M <ian@telem.us>
2020-04-28 22:05:36 -0500

    Bugfix

--- subnetget.py
+++ subnetget.py
@@ -9,6 +9,72 @@ def get_subnets(vpc, region_nm, profile_nm, file_name, public):
 
     with open(file_name, 'r+') as file:
         file.read()
+        print("subnet_id = {", file=file)
+        # PUBLIC SUBNETS
+        if public:
+            subnet_id = ec2.describe_subnets(
+                Filters=[
+                    {
+                        'Name': 'vpc-id',
+                        'Values': [
+                            vpc
+                        ]
+                    },
+                    {
+                        'Name': 'tag:Name',
+                        'Values': [
+                            '*ublic*'
+                        ]
+                    }
+                ]
+            )
+
+            # # Initial structure and defaults
+            subnet_dict = collections.defaultdict(dict)
+            subnet_dict['Mappings'] = collections.defaultdict(dict)
+            subnet_dict['Mappings']['SubnetMap'] = collections.defaultdict(dict)
+
+
+
+            for subnet in public_subnets['Subnets']:
+                subnet_dict['Mappings']['SubnetMap'][subnet['AvailabilityZone']]["Public"] = subnet['SubnetId']
+
+        else:
+            subnet_id = ec2.describe_subnets(
+                Filters=[
+                    {
+                        'Name': 'vpc-id',
+                        'Values': [
+                            vpc
+                        ]
+                    },
+                    {
+                        'Name': 'tag:Name',
+                        'Values': [
+                            '*rivate*'
+                        ]
+                    }
+                ]
+            )
+
+
+            # # Initial structure and defaults
+            subnet_dict = collections.defaultdict(dict)
+            subnet_dict['Mappings'] = collections.defaultdict(dict)
+            subnet_dict['Mappings']['SubnetMap'] = collections.defaultdict(dict)
+
+
+
+            for subnet in public_subnets['Subnets']:
+                subnet_dict['Mappings']['SubnetMap'][subnet['AvailabilityZone']]["Private"] = subnet['SubnetId']
+
+            # # The final printer
+            for key in subnet_dict:
+                for key_two in subnet_dict[key]:
+                    for key_three in subnet_dict[key][key_two]:
+                        for key_four in subnet_dict[key][key_two][key_three]:
+                            print(f'\t"{key_three}"\t= "{subnet_dict[key][key_two][key_three][key_four]}"', file=file)
+            print("}", file=file)
 
         # PRIVATE SUBNETS
 
@@ -53,44 +119,4 @@ def get_subnets(vpc, region_nm, profile_nm, file_name, public):
                         print(f'\t"{key_three}"\t= "{subnet_dict[key][key_two][key_three][key_four]}"', file=file)
         print("}", file=file)
 
-        # PUBLIC SUBNETS
-        if public:
-
-            print("subnets_public = {", file=file)
-
-            public_subnets = ec2.describe_subnets(
-                Filters=[
-                    {
-                        'Name': 'vpc-id',
-                        'Values': [
-                            vpc
-                        ]
-                    },
-                    {
-                        'Name': 'tag:Name',
-                        'Values': [
-                            '*ublic*'
-                        ]
-                    }
-                ]
-            )
-
-
-            # # Initial structure and defaults
-            subnet_dict = collections.defaultdict(dict)
-            subnet_dict['Mappings'] = collections.defaultdict(dict)
-            subnet_dict['Mappings']['SubnetMap'] = collections.defaultdict(dict)
-
-
-
-
-            for subnet in public_subnets['Subnets']:
-                subnet_dict['Mappings']['SubnetMap'][subnet['AvailabilityZone']]["Public"] = subnet['SubnetId']
-
-            # # The final printer
-            for key in subnet_dict:
-                for key_two in subnet_dict[key]:
-                    for key_three in subnet_dict[key][key_two]:
-                        for key_four in subnet_dict[key][key_two][key_three]:
-                            print(f'\t"{key_three}"\t= "{subnet_dict[key][key_two][key_three][key_four]}"', file=file)
-            print("}", file=file)
+        
