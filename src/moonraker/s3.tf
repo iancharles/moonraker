998e2c2f88b6bf766f6ae0be9af499fdba811016
Ian M <ian@telem.us>
2020-07-01 21:36:20 -0500

    Add encryption to S3 bucket

--- src/moonraker/s3.tf
+++ src/moonraker/s3.tf
@@ -13,6 +13,15 @@ resource "aws_s3_bucket" "VAR_NAME" {
     target_bucket = "VAR_LOGS"
     target_prefix = "VAR_NAME/"
   }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
 }
+
 
 
